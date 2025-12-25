c33998bdabda571f22febbc4d3988c164cf56981
FOURCLUBPHILE <fourclubphile@gmail.com>
2025-12-26 01:36:19 +0530

    feat: disable button while loading emoji data understood async

--- script1.js
+++ script1.js
@@ -4,11 +4,18 @@ const emojiNameEl = document.getElementById("emoji-name");
 const emoji = [];
 
 async function getEmoji() {
+
+
+
+  btnEl.disabled = true;
+  btnEl.innerText = "Loading...";
+
+
   let response = await fetch(
     "https://emoji-api.com/emojis?access_key=773b58f681fb786fafdb8392e8b8a75ddc177fd1"
   );
 
-  data = await response.json();
+  const data = await response.json();
 
   for (let i = 0; i < 1500; i++) {
     emoji.push({
@@ -16,6 +23,13 @@ async function getEmoji() {
       emojiCode: data[i].unicodeName,
     });
   }
+
+
+
+ // enabling  button sfter async work is done
+  btnEl.disabled = false;
+  btnEl.innerText = "Get Emoji";
+
 }
 
 getEmoji();
